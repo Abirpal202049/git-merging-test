f4943545f27860c3472e00837cda3399a8b61795
Abir Pal <abirpal282002@gmail.com>
2025-06-24 23:31:14 +0530

    Added multiply function

--- index.ts
+++ index.ts
@@ -5,3 +5,7 @@ function add(a: number, b: number): number {
 function subtract(a: number, b: number): number {
     return a - b;
 }
+
+function multiply(a: number, b: number): number {
+    return a * b;
+}
