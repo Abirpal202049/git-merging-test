e4e019a19518e1c446925a9cd3d9ea30d0804374
Abir Pal <abirpal282002@gmail.com>
2025-06-24 23:39:35 +0530

    Added Divide function

--- index.ts
+++ index.ts
@@ -9,3 +9,10 @@ function subtract(a: number, b: number): number {
 function multiply(a: number, b: number): number {
     return a * b;
 }
+
+function divide(a: number, b: number): number {
+    if (b === 0) {
+        throw new Error("Division by zero is not allowed.");
+    }
+    return a / b;
+}
